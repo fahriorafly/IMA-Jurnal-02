fe653f0891a332b0332bcf205a8c3567f78ff32b
fahriorafly <70989448+fahriorafly@users.noreply.github.com>
2020-09-21 19:15:49 +0700

    TugasJurnal.java

---
+++ Praktikum 2/TugasJurnal.java
@@ -0,0 +1,14 @@
+public class TugasJurnal {
+
+	public static void main(String[] args) {
+		int a = 222;
+		int b = 3;
+		int c = a * b;
+		
+		System.out.println("Nama		: Fahrio Rafly Albantani");
+		System.out.println("Panggilan 	: Fahri / Rio");
+		System.out.println("NIM		: 6706200093");
+		System.out.println("Kelas		: D3IF-44-01");
+		System.out.println(a + " * " + b + " = " + c); 
+	}
+}		
